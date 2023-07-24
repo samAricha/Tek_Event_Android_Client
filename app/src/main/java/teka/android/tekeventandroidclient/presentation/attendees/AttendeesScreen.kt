bd61aedc1f1d4dcac4930ab1e8ee903afdfb8d84
samAricha <samaricha001@gmail.com>
2023-07-24 22:37:40 +0300

    add fab for going to record screen

--- app/src/main/java/teka/android/tekeventandroidclient/presentation/attendees/AttendeesScreen.kt
+++ app/src/main/java/teka/android/tekeventandroidclient/presentation/attendees/AttendeesScreen.kt
@@ -7,12 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,6 +27,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOn
 import teka.android.tekeventandroidclient.data.room.models.EventVisitor
 import teka.android.tekeventandroidclient.presentation.guestRegistration.GuestRegistrationViewModel
+import teka.android.tekeventandroidclient.ui.components.SearchComposable
 
 @SuppressLint("FlowOperatorInvokedInComposition", "UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -31,8 +39,32 @@ fun AttendeesScreen(){
         .flowOn(Dispatchers.Main)
         .collectAsState(initial = emptyList())
 
-    Scaffold {
-        VisitorsList(eventVisitors = visitorsState)
+    var searchQuery by remember { mutableStateOf("") }
+
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                // Define your action for adding a visitor here
+                // For example, navigate to another composable/screen or show a dialog
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Add Visitor")
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Visitors List",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h6
+            )
+            SearchComposable { query ->
+                searchQuery = query
+            }
+            VisitorsList(eventVisitors = visitorsState)
+        }
     }
 
 }
@@ -42,13 +74,7 @@ fun AttendeesScreen(){
 fun VisitorsList(eventVisitors: List<EventVisitor>) {
 
     Column(
-        modifier = Modifier.padding(16.dp)
     ) {
-        Text(
-            text = "Visitors List",
-            modifier = Modifier.padding(bottom = 8.dp),
-            style = MaterialTheme.typography.h6
-        )
         LazyColumn(
             contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
 
--- app/src/main/java/teka/android/tekeventandroidclient/utils/sms_service/AppSmsSender.kt
+++ app/src/main/java/teka/android/tekeventandroidclient/utils/sms_service/AppSmsSender.kt
@@ -7,11 +7,6 @@ import javax.inject.Inject
 
 class AppSmsSender @Inject constructor(private val applicationContext: Context) : SmsSender {
     override fun sendSms(number: String, message: String) {
-//        val smsManager = SmsManager.getDefault()
-//        smsManager.sendTextMessage(number, null, message, null, null)
-
-//        val smsManager:SmsManager = applicationContext.getSystemService(SmsManager::class.java)
-
         val smsManager = SmsManager.getDefault()
         smsManager.sendTextMessage("+254708392326", null, message, null, null)
     }
